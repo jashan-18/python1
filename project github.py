3e47f8ad3900c01581b2bb803d3b072568ed1002
Annugill <118928025+Annugill@users.noreply.github.com>
2022-11-24 12:46:44 +0530

    Add files via upload

---
+++ project github.py
@@ -0,0 +1,14 @@
+# to declare a class employee which contains 2 methods: get_detail, forget the name ,employee_id,salary of employee and another method is print details
+class employee:
+    def get_details(self,n,i,s):
+        self.name=n
+        self.id=i
+        self.sal=s
+    def print_details(self):
+        return self.name+' '+self.id+' '+self.sal
+e1=employee()
+e1.get_details('Mohit','01','1234546543')
+e2=employee()
+e2.get_details('Rohit','02','9876543')
+print(e1.print_details())
+print(e2.print_details())
